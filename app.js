1471df3095fc71b7604dfb82eaaf23cd99a787a5
yebin <yebin5127@gmail.com>
2022-02-10 23:37:12 +0900

    UPDATE main

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const { sequelize } = require("./models");
 const router = require("./routes/index");
 const PORT = process.env.PORT || 3000;
@@ -9,9 +11,15 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
+
 app.use("/", router);
 
 app.set("jwt-secret", process.env.JWT_KEY);
+
+app.get("/", (req, res) => {
+    res.send(data);
+})
 
 sequelize.sync({ force : false })
     .then(() => {
